3eba9c5abc96139331fbfdd6c38d5f350008d864
PinoGabriel <pinogabriel01@gmail.com>
2023-12-11 15:16:29 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script>
+import AppComponent from './components/AppComponent.vue'
+import { store } from './store.js'
+import axios from 'axios'
+
+export default {
+	components: {
+		AppComponent,
+	},
+	data() {
+		return {
+			store,
+		}
+	},
+	methods: {
+
+	}
+}
+</script>
+
+<template>
+	<header>
+	</header>
+	<main>
+		<AppComponent />
+	</main>
+	<footer></footer>
+</template>
+
+<style lang="scss">
+@use './styles/general.scss';
+</style>
+
+<style scoped lang="scss">
+@use './styles/partials/variables' as *;
+</style>
---
+++ src/components/AppComponent.vue
@@ -0,0 +1,21 @@
+<script>
+import { store } from '../store.js'
+export default {
+	name: "AppHeader",
+	data() {
+		return {
+			store
+		}
+	},
+	methods: {
+	}
+}
+</script>
+
+<template>
+	<h1>Template</h1>
+</template>
+
+<style scoped lang="scss">
+@use '../styles/partials/variables' as *;
+</style>
